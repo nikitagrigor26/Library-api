72fd1e0b2e6fbf288e84c4f895cec570def28965
sfn4k <ghikita10@gmail.com>
2026-06-17 08:10:27 +0300

    Добавил middelware

--- Library-api/Main.go
+++ Library-api/Main.go
@@ -1,15 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	InitBook()
 	mux := http.NewServeMux()
+	loggegMux := LogingMiddelware(mux)
 	mux.HandleFunc("GET /", HelloUserHandler)
 	mux.HandleFunc("GET /books", BooksHandler)
 	mux.HandleFunc("GET /book/{id}", BookHandler)
 	mux.HandleFunc("PUT /add/book", addBookHandler)
 	mux.HandleFunc("DELTE /delete/book/{id}", deleteBookHandler)
-	
-	http.ListenAndServe(":8080", mux)
+	fmt.Println("Сервер запущен по адресу : http://localhost:8000")
+	http.ListenAndServe(":8000", loggegMux)
 }
